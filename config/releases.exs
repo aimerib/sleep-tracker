670810843107c2ae3792fd60e5bd9ee502b2d19a
Aimeri Baddouh <aimeri@baddouh.me>
2020-11-10 21:16:51 -0500

    fix db config for gigalixir

--- config/releases.exs
+++ config/releases.exs
@@ -10,7 +10,7 @@ database_url =
 config :sleep_tracker, SleepTracker.Repo,
   url: database_url,
   ssl: true,
-  ssl_opts: [cacertfile: '/app/certs/ca-certificate.crt'],
+  # ssl_opts: [cacertfile: '/app/certs/ca-certificate.crt'],
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
