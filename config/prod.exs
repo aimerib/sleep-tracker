5a51a6c9b832c8cd8e665cd29c14414dac838d18
Aimeri Baddouh <aimeri@baddouh.me>
2020-11-10 22:01:58 -0500

    switch to mix for gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -1,9 +1,38 @@
 use Mix.Config
 
-config :sleep_tracker, SleepTrackerWeb.Endpoint, server: true
+# config :sleep_tracker, SleepTrackerWeb.Endpoint, server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :sleep_tracker, SleepTracker.Repo,
+  url: {:system, "DATABASE_URL"},
+  database: "",
+  ssl: true,
+  # url: database_url,
+  # ssl: true,
+  # ssl_opts: [cacertfile: '/app/certs/ca-certificate.crt'],
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :sleep_tracker, SleepTrackerWeb.Mailer,
+  from: "support@mail.sleeptracker.io",
+  adapter: Swoosh.Adapters.SparkPost,
+  api_key: System.get_env("SPARKPOST_API"),
+  endpoint: "https://api.sparkpost.com/api/v1"
+
+# application_port = System.get_env("APP_PORT") || "4000"
+
+config :sleep_tracker, SleepTrackerWeb.Endpoint,
+  http: [port: 4000],
+  url: [scheme: "https", host: "www.sleeptracker.io", port: 443],
+  secret_key_base: secret_key_base
 
 # ## SSL Support
 #
